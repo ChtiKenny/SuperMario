4cfda95415a34b61d89c2e8ad82ea84ae1fb572f
ChtiKenny <ckenny.js@gmail.com>
2022-09-30 14:37:06 +0200

    episode 2 finished

---
+++ public/js/main.js
@@ -0,0 +1,42 @@
+import Compositor from './Compositor.js'
+import { loadLevel } from './loader.js'
+import { loadMarioSprite, loadBackgroundSprites } from './sprites.js'
+import { createBackgroundLayer } from './layers.js'
+
+const canvas = document.getElementById('screen')
+const context = canvas.getContext('2d')
+
+
+
+function createSpriteLayer(sprite, pos) {
+    return function drawSpriteLayer(context) {
+        sprite.draw('idle', context, pos.x, pos.y)
+    }
+}
+
+Promise.all([
+    loadMarioSprite(),
+    loadBackgroundSprites(),
+    loadLevel('1-1'),
+])
+.then(([marioSprite, backgroundSprites, level]) => {
+    const compositor = new Compositor()
+
+    const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites)
+    compositor.layers.push(backgroundLayer)
+
+    const pos = { x: 64, y:64}
+
+    const spriteLayer = createSpriteLayer(marioSprite, pos)
+    compositor.layers.push(spriteLayer)
+
+    function update() {
+        compositor.draw(context)
+        pos.x += 2
+        pos.y += 2
+        requestAnimationFrame(update)
+    }
+
+    update()
+})
+
