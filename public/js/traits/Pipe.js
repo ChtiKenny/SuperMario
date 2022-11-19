af84f42d261ff7472866041b64ac716265dbe7ca
ChtiKenny <ckenny.js@gmail.com>
2022-11-19 14:45:01 +0100

    Keep track of travelers in pipe

--- public/js/traits/Pipe.js
+++ public/js/traits/Pipe.js
@@ -5,19 +5,29 @@ import PipeTraveler from './PipeTraveler.js'
 export default class Pipe extends Trait {
     constructor() {
         super()
-        this.activated = false
+        this.travelers = new Set()
         this.direction = new Vector(0, 0)
     }
 
-    collides(us, them) {
-        if (this.activated) return
+    collides(pipe, traveler) {
+        if (!traveler.traits.has(PipeTraveler)) return
 
-        const pipeTraveler = them.traits.get(PipeTraveler)
-        if (pipeTraveler) {
-            console.log(pipeTraveler.direction)
-            if (pipeTraveler.direction.isEqual(this.direction)) {
-                us.sounds.add('pipe')
-                this.activated = true
+        if (this.travelers.has(traveler)) return
+
+        const pipeTraveler = traveler.traits.get(PipeTraveler)
+        console.log(pipeTraveler.direction)
+        if (pipeTraveler.direction.isEqual(this.direction)) {
+            console.log('Entering Pipe')
+            pipe.sounds.add('pipe')
+            this.travelers.add(traveler)
+        }
+    }
+
+    update(pipe, gameContext, level) {
+        for (const traveler of this.travelers) {
+            if (!traveler.bounds.overlaps(pipe.bounds)) {
+                console.log('Leaving Pipe')
+                this.travelers.delete(traveler)
             }
         }
     }
