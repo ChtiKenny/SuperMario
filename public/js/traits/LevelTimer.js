78e7798d8b46f0a486a8fe9380fe4f7592d4c817
ChtiKenny <ckenny.js@gmail.com>
2022-11-19 13:45:15 +0100

    Keep track if LevelTimer was used on level

--- public/js/traits/LevelTimer.js
+++ public/js/traits/LevelTimer.js
@@ -1,5 +1,7 @@
 import Trait from '../Trait.js'
 
+const MARK = Symbol('level timer earmark')
+
 export default class LevelTimer extends Trait {
     static EVENT_TIMER_HURRY = Symbol('timer hurry')
     static EVENT_TIMER_OK = Symbol('timer ok')
@@ -11,13 +13,18 @@ export default class LevelTimer extends Trait {
         this.hurryTime = 100
         this.hurryEmitted = null
     }
-    
+
     reset() {
         this.currentTime = this.totalTime;
     }
     
     update(entity, {deltaTime}, level) {
         this.currentTime -= deltaTime * 2.5
+
+        if(!level[MARK]) {
+            this.hurryEmitted = null
+        }
+
         if (this.hurryEmitted !== true && this.currentTime < this.hurryTime) {
             level.events.emit(LevelTimer.EVENT_TIMER_HURRY)
             this.hurryEmitted = true
@@ -27,5 +34,7 @@ export default class LevelTimer extends Trait {
             level.events.emit(LevelTimer.EVENT_TIMER_OK)
             this.hurryEmitted = false
         }
+
+        level[MARK] = true
     }
 }
