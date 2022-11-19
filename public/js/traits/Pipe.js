0be39d36a394a710398efae895a4842225dfd15c
ChtiKenny <ckenny.js@gmail.com>
2022-11-19 15:07:04 +0100

    Add Traveling animation to Pipe

--- public/js/traits/Pipe.js
+++ public/js/traits/Pipe.js
@@ -2,10 +2,19 @@ import Vector from '../Vector.js'
 import Trait from '../Trait.js'
 import PipeTraveler from './PipeTraveler.js'
 
+function createTravelerState() {
+    return {
+        time : 0,
+        start: new Vector(),
+        end: new Vector(),
+    }
+}
+
 export default class Pipe extends Trait {
     constructor() {
         super()
-        this.travelers = new Set()
+        this.duration = 1
+        this.travelers = new Map()
         this.direction = new Vector(0, 0)
     }
 
@@ -15,18 +24,26 @@ export default class Pipe extends Trait {
         if (this.travelers.has(traveler)) return
 
         const pipeTraveler = traveler.traits.get(PipeTraveler)
-        console.log(pipeTraveler.direction)
         if (pipeTraveler.direction.isEqual(this.direction)) {
-            console.log('Entering Pipe')
             pipe.sounds.add('pipe')
-            this.travelers.add(traveler)
+            
+            const state = createTravelerState()
+            state.start.set(traveler.position)
+            state.end.set(traveler.position)
+            state.end.x += this.direction.x * pipe.size.x
+            state.end.y += this.direction.y * pipe.size.y
+            this.travelers.set(traveler, state)
         }
     }
 
     update(pipe, gameContext, level) {
-        for (const traveler of this.travelers) {
-            if (!traveler.bounds.overlaps(pipe.bounds)) {
-                console.log('Leaving Pipe')
+        const {deltaTime} = gameContext
+        for (const [traveler, state] of this.travelers.entries()) {
+            state.time += deltaTime
+            const progress = state.time / this.duration
+            traveler.position.x = state.start.x + (state.end.x - state.start.x) * progress
+            traveler.position.y = state.start.y + (state.end.y - state.start.y) * progress
+            if (state.time > this.duration) {
                 this.travelers.delete(traveler)
             }
         }
